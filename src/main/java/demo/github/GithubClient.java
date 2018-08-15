c923f94c697e0c6e2f5d66cc39f44651fb0a367c
Stephane Nicoll <snicoll@pivotal.io>
2018-08-15 09:43:14 +0300

    Initial import using RestTemplate

---
+++ src/main/java/demo/github/GithubClient.java
@@ -0,0 +1,56 @@
+package demo.github;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+
+import demo.VaadinRestDemoProperties;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GithubClient {
+
+	private final RestTemplate restTemplate;
+
+	public GithubClient(RestTemplateBuilder restTemplateBuilder,
+			VaadinRestDemoProperties properties) {
+		this.restTemplate = setupRestTemplate(restTemplateBuilder,
+				properties.getGithub().getToken());
+	}
+
+	public List<Commit> getRecentCommits(String organization, String project) {
+		String url = String.format(
+				"https://api.github.com/repos/%s/%s/commits", organization, project);
+		ResponseEntity<Commit[]> response = this.restTemplate.exchange(createRequestEntity(url), Commit[].class);
+		return Arrays.asList(response.getBody());
+	}
+
+	private RequestEntity<?> createRequestEntity(String url) {
+		try {
+			return RequestEntity.get(new URI(url))
+					.accept(MediaType.APPLICATION_JSON).build();
+		}
+		catch (URISyntaxException ex) {
+			throw new IllegalStateException("Invalid URL " + url, ex);
+		}
+	}
+
+	private RestTemplate setupRestTemplate(RestTemplateBuilder builder, String token) {
+		if (StringUtils.hasText(token)) {
+			String[] content = token.split(":");
+			Assert.state(content.length == 2, "Invalid Github token");
+			builder = builder.basicAuthorization(content[0], content[1]);
+		}
+		return builder.build();
+	}
+
+}
---
+++ src/main/java/demo/ui/MainUI.java
@@ -0,0 +1,72 @@
+package demo.ui;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import demo.github.Commit;
+import demo.github.GithubClient;
+
+@PageTitle("Vaadin with RestTemplate demo")
+@Route("")
+public class MainUI extends VerticalLayout {
+
+	private static final long serialVersionUID = 1L;
+
+	private Grid<Commit> commits = new Grid<>();
+
+	private TextField organization = new TextField("Organization:", "vaadin", "");
+
+	private TextField project = new TextField("Project:", "spring", "");
+
+	private final GithubClient githubClient;
+
+	public MainUI(GithubClient c) {
+		this.githubClient = c;
+
+		commits.addComponentColumn(this::createLink).setFlexGrow(3).setHeader("Message");
+		commits.addColumn(commit -> commit.getCommitter().getName()).setFlexGrow(1).setHeader("Committer");
+		commits.setWidth("100%");
+
+		Button refresh = new Button("", this::refresh);
+		refresh.setIcon(VaadinIcon.REFRESH.create());
+		refresh.getElement().getThemeList().add("primary");
+
+		HorizontalLayout horizontalLayout = new HorizontalLayout(
+				organization, project, refresh
+		);
+		horizontalLayout.setVerticalComponentAlignment(Alignment.END, refresh);
+
+		add(
+				new Text("Vaadin with RestTemplate demo"),
+				horizontalLayout,
+				commits
+		);
+		expand(commits);
+		setSizeFull();
+		listCommits();
+	}
+
+	private void listCommits() {
+		commits.setItems(githubClient.getRecentCommits(
+				organization.getValue(), project.getValue()));
+	}
+
+	private void refresh(ClickEvent clickEvent) {
+		listCommits();
+	}
+
+	private Anchor createLink(Commit c) {
+		String url = String.format("https://github.com/%s/%s/commit/%s",
+				organization.getValue(), project.getValue(), c.getSha());
+		return new Anchor(url, c.getMessage());
+	}
+
+}
